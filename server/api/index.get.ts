b23f78b794a008ef730d85ad0126a11ac8d3c150
klyutko <klyutko.d@gmail.com>
2023-03-20 23:46:14 +0600

    add index.get

---
+++ server/api/index.get.ts
@@ -0,0 +1,15 @@
+// import { Schema, model } from "mongoose"
+
+// const UserSchema = new Schema({
+//     email: String,
+//     name: String,
+// })
+
+// export const User = model<any>('User', UserSchema)
+
+// export default defineEventHandler(async (event) => {
+
+//     const users = await User.find()
+
+//     return { users }
+// })
